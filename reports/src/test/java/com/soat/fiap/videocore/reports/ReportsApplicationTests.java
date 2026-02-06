76bc7321dfc36355eba8a169725b32687f4c5405
Pedro <ferrarezzolabs@outlook.com>
2026-02-06 15:54:39 -0300

    chore: remve @SpringBootTest annotation

--- reports/src/test/java/com/soat/fiap/videocore/reports/ReportsApplicationTests.java
+++ reports/src/test/java/com/soat/fiap/videocore/reports/ReportsApplicationTests.java
@@ -3,7 +3,7 @@ package com.soat.fiap.videocore.reports;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ReportsApplicationTests {
 
 	@Test
