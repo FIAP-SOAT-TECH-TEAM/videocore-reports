ce6ce8a63cf62e9e8debda945c4b7b1d2483e005
Pedro <ferrarezzolabs@outlook.com>
2026-02-13 11:49:49 -0300

    fix: define hint for RntbdToken to graalvm recognize class constructors

--- reports/src/main/java/com/soat/fiap/videocore/reports/infrastructure/common/hints/azure/cosmos/CosmosHints.java
+++ reports/src/main/java/com/soat/fiap/videocore/reports/infrastructure/common/hints/azure/cosmos/CosmosHints.java
@@ -3,8 +3,10 @@ package com.soat.fiap.videocore.reports.infrastructure.common.hints.azure.cosmos
 import com.azure.spring.data.cosmos.core.CosmosTemplate;
 import com.azure.spring.data.cosmos.repository.support.CosmosEntityInformation;
 import com.azure.spring.data.cosmos.repository.support.SimpleCosmosRepository;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 
 /**
  * Registrar de {@link RuntimeHints} para suporte a reflexão em tempo de execução
@@ -32,24 +34,22 @@ public class CosmosHints implements RuntimeHintsRegistrar {
 
         hints.reflection().registerType(
                 SimpleCosmosRepository.class,
-                builder -> builder.withMembers(
-                        org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_METHODS
-                )
+                builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS)
         );
 
         hints.reflection().registerType(
                 CosmosEntityInformation.class,
-                builder -> builder.withMembers(
-                        org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTRUCTORS
-                )
+                builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
         );
 
         hints.reflection().registerType(
                 CosmosTemplate.class,
-                builder -> builder.withMembers(
-                        org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTRUCTORS
-                )
+                builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+        );
+
+        hints.reflection().registerType(
+                TypeReference.of("com.azure.cosmos.implementation.directconnectivity.rntbd.RntbdToken$PropertyFilter"),
+                builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
         );
     }
 }
