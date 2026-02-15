2f047ab9db9c053972bf410ba6d4199526a88883
Pedro <ferrarezzolabs@outlook.com>
2026-02-15 10:13:59 -0300

    refactor: create JacksonHints for jackson serialization objects

--- reports/src/main/java/com/soat/fiap/videocore/reports/ReportsApplication.java
+++ reports/src/main/java/com/soat/fiap/videocore/reports/ReportsApplication.java
@@ -2,16 +2,13 @@ package com.soat.fiap.videocore.reports;
 
 import com.soat.fiap.videocore.reports.common.hints.azure.BlobHints;
 import com.soat.fiap.videocore.reports.common.hints.azure.CosmosHints;
-import com.soat.fiap.videocore.reports.core.interfaceadapters.dto.ProcessVideoErrorEventDto;
-import com.soat.fiap.videocore.reports.infrastructure.in.event.azsvcbus.payload.ProcessVideoStatusUpdatePayload;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import com.soat.fiap.videocore.reports.common.hints.jackson.JacksonHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints({CosmosHints.class, BlobHints.class})
-@RegisterReflectionForBinding({ProcessVideoStatusUpdatePayload.class, ProcessVideoErrorEventDto.class})
+@ImportRuntimeHints({CosmosHints.class, BlobHints.class, JacksonHints.class})
 public class ReportsApplication {
 
     static void main(String[] args) {
