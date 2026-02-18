ffc3ee184d8dbe0061283f69de12294ede1b2d27
Pedro <ferrarezzolabs@outlook.com>
2026-02-18 00:07:31 -0300

    fix: use graal vm tracing agent to generate reachability-metadata file

--- reports/src/main/java/com/soat/fiap/videocore/reports/ReportsApplication.java
+++ reports/src/main/java/com/soat/fiap/videocore/reports/ReportsApplication.java
@@ -1,14 +1,9 @@
 package com.soat.fiap.videocore.reports;
 
-import com.soat.fiap.videocore.reports.common.hints.azure.BlobHints;
-import com.soat.fiap.videocore.reports.common.hints.azure.CosmosHints;
-import com.soat.fiap.videocore.reports.common.hints.jackson.JacksonHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints({CosmosHints.class, BlobHints.class, JacksonHints.class})
 public class ReportsApplication {
 
     static void main(String[] args) {
